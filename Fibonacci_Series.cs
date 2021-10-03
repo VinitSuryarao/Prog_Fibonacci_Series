4d8e678b46fe3570fb1a4f11ee0e1526e6b1d870
Vinit Suryarao <suryarao.vinit@hotmail.com>
2021-10-03 19:00:55 +0530

    Add files via upload

---
+++ Fibonacci_Series.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Program
+    {
+        //   Fibonacci Series
+        // Number = 5 then Seris is 0 1 1 2 3
+        // Sum of two number will be next number
+
+        static void Main(string[] args)
+        {
+            int n=6, a = 0, b = 1, temp;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(a);
+                temp = a;
+                a = b;
+                b = temp + b;
+            }
+            
+        }
+    }
+
+
